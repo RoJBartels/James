fc61eb77428fcac378cb1a70232393bed1846eeb
RoJBartels <robinjens.bartels@gmail.com>
2026-05-08 22:09:50 +0200

    list of capabilities

---
+++ routing/capabilities.py
@@ -0,0 +1,40 @@
+# james/routing/capabilities.py
+
+"""
+Semantische Capability-Namen für den FL1-Router.
+
+Capabilities beschreiben:
+    potentiell relevante Fähigkeitsräume
+
+Nicht:
+    konkrete Tools
+    konkrete Modelle
+    konkrete Ausführung
+    Governance-Entscheidungen
+"""
+
+# ------------------------------------------------------------------
+# System
+# ------------------------------------------------------------------
+
+SYSTEM_STATUS = "system_status"
+SYSTEM_INFO = "system_info"
+
+# ------------------------------------------------------------------
+# Research
+# ------------------------------------------------------------------
+
+RESEARCH_MEMORY = "research_memory"
+RESEARCH_WEB = "research_web"
+
+# ------------------------------------------------------------------
+# Project
+# ------------------------------------------------------------------
+
+PROJECT_PLANNING = "project_planning"
+
+# ------------------------------------------------------------------
+# Execution
+# ------------------------------------------------------------------
+
+EXECUTION_CODE = "execution_code"
